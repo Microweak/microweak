c531cf18e32cf29cac7799f062840be02403c10a
Microweak <wibusite@gmail.com>
2024-01-19 00:43:19 +0700

    Updated

---
+++ app.js
@@ -0,0 +1,29 @@
+import module from "./lib/modules.js";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// First
+global.__dirname = dirname(fileURLToPath(import.meta.url));
+const { M, config: conf } = module;
+
+(async function () {
+    conf(await (await M("path")).join(__dirname, "plugins"));
+    
+    /* [ get all object configuration ] */
+    const config = await import(await (await M("path")).join(__dirname, "config.json"), {
+        assert: {
+            type: "json"
+        }
+    });
+    const default_config = config.default ? config.default : config;
+    const config_keys = Object.keys( default_config );
+    const { port } = default_config;
+    /* [ End object configuration ] */
+
+    const express = await M("express");
+    const app = express();
+    app.all("*", await M( "controller" ))
+
+    app.listen( port.http );
+    app.listen( port.https );
+})();
---
+++ header.js
@@ -0,0 +1,6 @@
+/**
+  * © [2023 - ${new Date().getFullYear()}] Paukuman Groups
+  * Microweak is Free and Open Source lightweight CMS like Wordpress for Nodejs
+  * All Rights Reserved.
+  * Documentation : paukuman.github.io
+**/
