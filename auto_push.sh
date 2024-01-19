1f8e39fd461f5af753c35fd2ae7b19e61111df8a
Microweak <152780262+Microweak@users.noreply.github.com>
2024-01-20 05:40:37 +0700

    Update auto_push.sh

--- auto_push.sh
+++ auto_push.sh
@@ -1,7 +1,1 @@
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/termux_github
-ssh -T git@github.com
-git remote set-url origin git@github.com:Microweak/microweak.git
-git add -A
-git commit -am "Update"
-git push
+
